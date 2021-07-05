4ea3174165614d8a10f99f8d9eb40d561524e0a8
Vlad <vlad.golovchak.1996@gmail.com>
2021-07-05 19:30:39 +0300

    Associates products and orders with a placements model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@
 
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :orders, through: :placements
 
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
@@ -19,7 +21,6 @@ class Product < ApplicationRecord
     products = products.above_or_equal_to_price(params [:min_price].to_f) if params[:min_price]
     products = products.below_or_equal_to_price(params [:max_price].to_f) if params[:max_price]
     products = products.recent if params[:recent]
-
     products
   end
 end
